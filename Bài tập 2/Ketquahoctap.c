cbe424ec6fe6f0f81ba31fe8811e9dd36ad22207
Phat25082002 <72543407+Phat25082002@users.noreply.github.com>
2020-12-03 16:32:44 +0700

    Create Ketquahoctap.c

---
+++ Bài tập 2/Ketquahoctap.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+  // khai bao bien V va T;
+  int V, T;
+  // hien thi nhap V =
+  printf (" nhap Van = ");
+  scanf (" %d", &V);
+  // hien thi nhap T =
+  printf (" nhap Toan = ");
+  scanf ("%d", &T);
+  // Tinh diem trung binh Van, Toan va hien ra man hinh V + T/2 = DTB
+  printf ("Van + Toan/2 = DBT");
+  printf ("\n");
+  printf ("Van \t Toan \t DTB");
+  return 0;
+}
