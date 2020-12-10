a9137b10057878b3d2611845c2173fe9bd4065e3
Phat25082002 <72543407+Phat25082002@users.noreply.github.com>
2020-12-10 15:39:42 +0700

    Create sosanh.c

---
+++ Bài tập 3/sosanh.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void) {
+  // khai bao bien a,b;
+  int a,b;
+  // hien thi ra man hinh a
+  printf (" nhap a = ");
+  scanf ("%d", &a);
+  // hien thi ra man hinh b
+  printf (" nhap b = ");
+  scanf ("%d", &b);
+  if (a>b){
+    // hien thi ra man hinh a lon hon b
+    printf ("a>b", a,b);
+    scanf ("%d", &a);
+  }else if (a == b) {
+    // hien thi ra man hinh a bang b
+    printf ("a=b", a,b);
+    scanf ("%d", &a);
+  }else {
+    // hien thi ra man hinh a be hon b
+    printf ("a<b", a,b);
+    scanf ("%d", &a);
+  }
+    getchar ();
+    return 0;
+  }
