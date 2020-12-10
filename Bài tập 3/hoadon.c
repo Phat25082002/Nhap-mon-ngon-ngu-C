7d917926d34d9e08cb5f99f6815cb4ba7d2edd16
Phat25082002 <72543407+Phat25082002@users.noreply.github.com>
2020-12-10 16:56:18 +0700

    Create hoadon.c

---
+++ Bài tập 3/hoadon.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+  // khai bao 2 bien DG vs SL
+  int DG,SL;
+  float TT;
+  if (SL>5=-25%) {
+  // hien thi ra man hinh dongia =
+  printf (" DG = ");
+  scanf ("%d", &DG);
+  // hien thi ra man hinh so luong =
+  printf (" SL = ");
+  scanf ("%d" ,&SL);
+  } else (SL<5=25%) {
+    printf ("SL<5=25%",SL);
+    scanf ("%d", &SL);
+  TT = (SL + DG)/100*25;
+  // tinh so luong thanh tien
+  printf ("(SL + DG)/100*25= %d",(DG + SL)/100*25);
+  }
+  getchar ();
+  return 0;
+  }
