058e4e7366845a1512d66c3c87a6f59fae006ef4
Phat25082002 <72543407+Phat25082002@users.noreply.github.com>
2020-12-10 15:05:22 +0700

    Update amduong.c

--- Bài tập 3/amduong.c
+++ Bài tập 3/amduong.c
@@ -4,14 +4,17 @@ int main(void) {
   // khai bao bien a
   int a = -1;
   if (a > 0){
-    // hien thi bien a la so duong;
+    // hien thi bien a la so duong
     printf ("%d duong", a);
+    scanf ("%d", &a);
   }else if (a == 0) {
-    // hien thi bien a bang 0;
+    // hien thi bien a bang 0
     printf ("%d", a);
+    scanf ("%d", &a);
   }else {
-    // hien thi bien a la so am;
+    // hien thi bien a la so am
     printf ("%d am", a);
+    scanf ("%d", &a);
   }
     getchar ();
     return 0;
