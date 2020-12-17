43f4b6d9c71f135518a1f6d146691219dfbf662c
Phat25082002 <72543407+Phat25082002@users.noreply.github.com>
2020-12-17 13:57:00 +0700

    Update hoadon.c

--- Bài tập 3/hoadon.c
+++ Bài tập 3/hoadon.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
 int main(void) {
-  // khai bao bien donggia, soluong, thanhtien
-  int dongia, soluong, thanhtien;
+  // khai bao bien dongia, soluong, thanhtien
+  int DG, SL, TT;
   // hien thi ra man hinh dongia =
-  printf (" dongia = ");
-  scanf ("%d", &dongia);
+  printf (" DG = ");
+  scanf ("%d", &DG);
   // hien thi ra man hinh so luong =
-  printf (" soluong = ");
-  scanf ("%d" ,&soluong);
-  thanhtien = dongia * soluong;
-  // soluong >= 5 thi don gia se giam 50%
-  if (soluong >=5 & dongia >=5){
+  printf (" SL = ");
+  scanf ("%d" ,&SL);
+  TT = DG * SL;
+  // soluong >= 5 va dongia >=500 thi thanhtien se giam 50%
+  if (SL >=5 & DG >=500){
   // hien thi ra so tien can phai thanh toan khi soluong >=5
-  printf ("so tien can phai thanh toan la %d", thanhtien*50/100);
+  printf ("so tien can phai thanh toan la %d", TT*50/100);
   } else {
   // hien ra so tien can phai thanh toan khi don soluong <5
-  printf (" so tien can phai thanh toan la %d", thanhtien);}
+  printf (" so tien can phai thanh toan la %d", TT);}
   return 0;
   getchar ();
 }
