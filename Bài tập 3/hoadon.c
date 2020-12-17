da3c8c7e0fa53be4e9cd09f7f22f86cce17b479d
Phat25082002 <72543407+Phat25082002@users.noreply.github.com>
2020-12-17 14:25:07 +0700

    Update hoadon.c

--- Bài tập 3/hoadon.c
+++ Bài tập 3/hoadon.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
 int main(){
-	// khai bao bien don gia,so luong,thanh tien
+ // khai bao bien don gia,so luong,thanh tien
 	int dongia,soluong,thanhtien;
-  // hien thi ra man hinh dongia
+ // hien thi ra man hinh dongia
 	printf ("dongia =");
 	scanf("%d",& dongia) ;
-  // hien thi ra man hinh soluong
+ // hien thi ra man hinh soluong
 	printf ("soluong =");
 	scanf("%d",& soluong);
-  // cong thuc
+ // cong thuc
 	thanhtien = soluong*dongia;
-	// dieu kien so luong >=5 va dongia >=500
-	if (soluong >= 5 & dongia >=500 ){
+ // dieu kien so luong >=5 va dongia >=500
+if (soluong >= 5 & dongia >=500 ){
     thanhtien= (soluong*dongia)*0.5;
-    // hien thi ra man hinh thanhtien
-		printf ("%d",thanhtien );
-	}else {
-		printf (" %d",thanhtien);
+ // hien thi ra man hinh thanhtien
+	printf ("%d",thanhtien );
+}else {
+	printf (" %d",thanhtien);
 	}
   getchar();
   return 0;
