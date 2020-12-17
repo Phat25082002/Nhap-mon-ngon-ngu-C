fcbad1ce3533ab3a3c7de2e25bec583b703f48a1
Phat25082002 <72543407+Phat25082002@users.noreply.github.com>
2020-12-17 15:06:06 +0700

    Create note.c

---
+++ note.c
@@ -0,0 +1,6 @@
+Dấu &: dùng để hiện thị vị trí, địa chỉ của tên biến đó
+==: là so sánh 
+=: là gán 
+int a: là muốn máy tính trình phát 1 ô nhớ mang tên a chứa kiểu dữ liệu có kích thước và vị trí 
+%d: cho số nguyên
+%f: cho số thực
