1539cc7c777c3bbf482f38c224946acee6f1bdeb
Phat25082002 <72543407+Phat25082002@users.noreply.github.com>
2020-12-24 13:41:51 +0700

    Create xinchao.c

---
+++ Bài tập 3/xinchao.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  int i;
+  for (i = 1; i < 11; i++)
+  {
+    printf("%d ", i);
+    // hien thi ra man hinh 10 dong xin chao
+    printf ("xinchao\n");
+  }
+  return 0;
+}
