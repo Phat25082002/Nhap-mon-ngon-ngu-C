925d64e87b739a6f43e36d9366bad5afdc47c9b5
Phat25082002 <72543407+Phat25082002@users.noreply.github.com>
2020-12-24 15:05:09 +0700

    Create tong10.c

---
+++ Bài tập 3/tong10.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  // khai bao bien i, Tong
+  int i,Tong; 
+  {
+    printf ("%.d ",i);
+    // cong thuc tinh Tong
+    Tong = 1+2+3+4+5+6+7+8+9+10;
+    // hien thi ra man hinh Tong
+    printf ("Tong la %.d",Tong);
+  }
+  return 0;
+}
