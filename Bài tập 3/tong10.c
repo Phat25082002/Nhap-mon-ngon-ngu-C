0d910228d5c4e0ee8d0159660dc5d2db91186391
Phat25082002 <72543407+Phat25082002@users.noreply.github.com>
2020-12-24 15:55:38 +0700

    Update tong10.c

--- Bài tập 3/tong10.c
+++ Bài tập 3/tong10.c
@@ -1,3 +1,4 @@
+Cách 1:
 #include <stdio.h>
 
 int main() {
@@ -10,5 +11,18 @@ int main() {
     // hien thi ra man hinh Tong
     printf ("Tong la %.d",Tong);
   }
+  return 0;
+}
+
+Cách 2:
+#include <stdio.h>
+
+int main() {
+  // khai bao bien i, Tong
+  int tong= 0;
+  for (int dem = 1; dem <= 10; dem++){
+    tong = tong + dem;
+  }
+  printf ("Tong la %d", tong);
   return 0;
 }
