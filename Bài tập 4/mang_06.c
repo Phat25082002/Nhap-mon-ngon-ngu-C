900512cc9d21e4583e95fcbc448043f472e4f6d9
Phat25082002 <72543407+Phat25082002@users.noreply.github.com>
2021-01-21 15:55:27 +0700

    Create mang_06.c

---
+++ Bài tập 4/mang_06.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void){
+  int n; // khai bao bien n
+  int a[n]; // khai bao hop n ngan
+  printf ("Nhap n:"); // nhap n
+  scanf ("%d",&n);
+  for (int i=1; i<=n; i++){
+    printf ("Phan tu o mang a[%d]:",i); // hien thi ra man hinh cac mang
+    scanf ("%d",&a[i]);
+  }
+  int m; // khai bao bien m
+  printf ("Nhap m:"); // nhap m
+  scanf ("%d",&m);
+  for (int j=0; j <m; j++){
+  printf ("So phan tu co gia tri %d:%d\n",m,m+1); // hien thi ra man hinh so cac pt
+  printf ("Chi so cac phan tu tren la: %d %d",j,m); // hien thi ra man hinh chi so cac pt
+  if (n % m){
+    printf ("Khong ton tai"); // hien thi thong diep khong ton tai
+    scanf ("%d",&n);
+ } else {
+   printf ("\n");
+    printf ("Co ton tai"); // hien thi thong diep co ton tai
+    scanf ("%d",&n);
+ }
+ }
+return 0;
+}
