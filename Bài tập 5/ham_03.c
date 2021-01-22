69c0a83934b165c12ca89ba20887fbf8964cf8b2
Phat25082002 <72543407+Phat25082002@users.noreply.github.com>
2021-01-22 10:57:21 +0700

    Create ham_03.c

---
+++ Bài tập 5/ham_03.c
@@ -0,0 +1,26 @@
+-------- Chưa làm xong, chưa hoàn thiện---------
+//Khoi khai bao
+#include<stdio.h>
+int Nhap_pt_mang();
+int Hien_thi_pt_mang ();
+int Hien_thi_tong_chan();
+int Dem_chan_le_0 ();
+//khoi ham main
+int main(){
+int n;
+printf("Nhap n = ");
+scanf("%d", &n);
+int a[n];
+Nhap_pt_mang(a);
+Hien_thi_pt_mang(a);
+Hien_thi_tong_chan(a);
+Dem_chan_le_0(a);
+return 0;
+}
+int Nhap_pt_mang(){
+int Hien_thi_pt_mang();
+int Hien_thi_tong_chan();
+int Dem_chan_le_0();
+  return 0;
+}
+-------- Chưa làm xong ---------
