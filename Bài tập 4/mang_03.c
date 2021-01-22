519b159b07f02ef6ba60c88ecda9212c72af8e05
Phat25082002 <72543407+Phat25082002@users.noreply.github.com>
2021-01-22 15:27:41 +0700

    Create mang_03.c

---
+++ Bài tập 4/mang_03.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+  int n,i; // khai bao 
+  int a[n]; // khai bao hop n ngan
+  printf ("Nhap n:"); // nhap n
+  scanf ("%d",&n);
+  for ( i=0; i<=n; i++){
+    printf ("a[%d]: %d\n",i,i+1); // hien thi ra man hinh so mang
+  }
+  return 0;
+}
